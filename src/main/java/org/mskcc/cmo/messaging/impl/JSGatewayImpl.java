6372f8684027b66916af1bc7db9c7507eb4b4826
Angelica Ochoa <ochoaa@mskcc.org>
2021-04-14 14:29:25 -0400

    Refactored publishing
    Signed-off-by: Angelica Ochoa <ochoaa@mskcc.org>

--- src/main/java/org/mskcc/cmo/messaging/impl/JSGatewayImpl.java
+++ src/main/java/org/mskcc/cmo/messaging/impl/JSGatewayImpl.java
@@ -3,17 +3,11 @@ package org.mskcc.cmo.messaging.impl;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.nats.client.Connection;
-import io.nats.client.Connection.Status;
 import io.nats.client.Dispatcher;
 import io.nats.client.JetStream;
-import io.nats.client.JetStreamOptions;
 import io.nats.client.JetStreamSubscription;
 import io.nats.client.Message;
-import io.nats.client.MessageHandler;
 import io.nats.client.Nats;
-import io.nats.client.api.PublishAck;
-import io.nats.client.impl.NatsMessage;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -30,150 +24,69 @@ import java.util.concurrent.TimeUnit;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.mskcc.cmo.common.FileUtil;
+import org.mskcc.cmo.common.impl.FileUtilImpl;
 import org.mskcc.cmo.messaging.Gateway;
 import org.mskcc.cmo.messaging.MessageConsumer;
-import org.mskcc.cmo.messaging.utils.SSLUtils;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mskcc.cmo.messaging.events.NATSPublisher;
+import org.mskcc.cmo.messaging.events.PublishingQueueTask;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
-@Primary
 @Component
 public class JSGatewayImpl implements Gateway {
-    @Value("${nats.tls_channel:false}")
-    private boolean tlsChannel;
+    private static final Log LOG = LogFactory.getLog(JSGatewayImpl.class);
+    private static final CountDownLatch PUBLISHING_SHUTDOWN_LATCH = new CountDownLatch(1);
+    private static volatile Boolean shutdownInitiated = Boolean.FALSE;
+    private static volatile BlockingQueue<PublishingQueueTask> PUBLISHING_QUEUE = new LinkedBlockingQueue<>();
+    private final ObjectMapper mapper = new ObjectMapper();
+    private final Map<String, JetStreamSubscription> subscribers = new HashMap<>();
+    private volatile ExecutorService exec = Executors.newSingleThreadExecutor();
 
+    // connection variables
     @Value("${nats.url}")
-    private String natsURL;
+    public String natsUrl;
 
+    private Connection natsConnection;
+    private JetStream jsConnection;
+
+    // publishing logger file variables
     @Value("${metadb.publishing_failures_filepath}")
     private String metadbPubFailuresFilepath;
 
-    private FileUtil fileUtil;
-
-    private File pubFailuresFile;
-
-    @Autowired
-    private void initPubFailuresFile() throws IOException {
-        this.pubFailuresFile = fileUtil.getOrCreateFileWithHeader(
-                metadbPubFailuresFilepath, PUB_FAILURES_FILE_HEADER);
-    }
-
-    @Autowired
-    SSLUtils sslUtils;
-
-    private static final String PUB_FAILURES_FILE_HEADER = "DATE\tTOPIC\tMESSAGE\n";
-    private Connection natsConnection;
-    private JetStream jsConnection;
-    private final ObjectMapper mapper = new ObjectMapper();
-    private final Map<String, JetStreamSubscription> subscribers = new HashMap<String,
-            JetStreamSubscription>();
-    private volatile boolean shutdownInitiated;
-    private final ExecutorService exec = Executors.newSingleThreadExecutor();
-    private final CountDownLatch publishingShutdownLatch = new CountDownLatch(1);
-    private final BlockingQueue<PublishingQueueTask> publishingQueue =
-        new LinkedBlockingQueue<PublishingQueueTask>();
-    private final Log LOG = LogFactory.getLog(JSGatewayImpl.class);
-
-    private class PublishingQueueTask {
-        String topic;
-        Object message;
-
-        PublishingQueueTask(String topic, Object message) {
-            this.topic = topic;
-            this.message = message;
-        }
-    }
-
-    private class NATSPublisher implements Runnable {
-
-        Connection natsConn;
-        JetStream jetStreamConn;
-        boolean interrupted = false;
-
-        NATSPublisher() throws Exception {
-            JetStreamOptions jsOptions = new JetStreamOptions.Builder()
-                    .build();
-            natsConn = Nats.connect(natsConnection.getOptions());
-            this.jetStreamConn = natsConn.jetStream(jsOptions);
-        }
-
-        @Override
-        public void run() {
-            while (true) {
-                try {
-                    PublishingQueueTask task = publishingQueue.poll(100, TimeUnit.MILLISECONDS);
-                    if (task != null) {
-                        String msg = mapper.writeValueAsString(task.message);
-                        try {
-                            PublishAck ack = jetStreamConn.publish(task.topic, msg.getBytes(StandardCharsets.UTF_8));
-                            if (ack.getError() != null) {
-                                fileUtil.writeToFile(pubFailuresFile,
-                                        generatePublishFailureRecord(task.topic, msg));
-                            }
-                        } catch (Exception e) {
-                            try {
-                                fileUtil.writeToFile(pubFailuresFile,
-                                        generatePublishFailureRecord(task.topic, msg));
-                            } catch (IOException ex) {
-                                LOG.error("Error during attempt to log publishing failure to file: "
-                                        + metadbPubFailuresFilepath, e);
-                            }
-                            if (e instanceof InterruptedException) {
-                                interrupted = true;
-                            } else {
-                                LOG.error("Error during attempt to publish on topic: " + task.topic, e);
-                            }
-                        }
-                    }
-                    if ((interrupted || shutdownInitiated) && publishingQueue.isEmpty()) {
-                        break;
-                    }
-                } catch (InterruptedException e) {
-                    interrupted = true;
-                } catch (JsonProcessingException e) {
-                    LOG.error("Error parsing JSON from message", e);
-                }
-            }
-            try {
-                natsConn.close();
-                publishingShutdownLatch.countDown();
-            } catch (Exception e) {
-                LOG.error("Error closing streaming connection: %s\n" + e.getMessage());
-            }
-        }
-    }
+    private final String PUB_FAILURES_FILE_HEADER = "DATE\tTOPIC\tMESSAGE\n";
+    private final static FileUtil fileUtil = new FileUtilImpl();
+    private static File publishingLoggerFile;
 
     @Override
     public void connect() throws Exception {
-        connect(natsURL);
+        connect(natsUrl);
     }
 
     @Override
     public void connect(String natsUrl) throws Exception {
-        natsConnection = Nats.connect(natsUrl);
-        jsConnection = natsConnection.jetStream();
-        exec.execute(new NATSPublisher());
+        this.natsConnection = Nats.connect(natsUrl);
+        this.jsConnection = natsConnection.jetStream();
+        publishingLoggerFile = fileUtil.getOrCreateFileWithHeader(
+                metadbPubFailuresFilepath, PUB_FAILURES_FILE_HEADER);
+        exec.execute(NATSPublisher.withDefaultOptions(natsConnection.getOptions()));
     }
 
     @Override
-    public boolean isConnected() {
+    public  boolean isConnected() {
         if (natsConnection == null) {
             return Boolean.FALSE;
         }
         return (natsConnection != null && natsConnection.getStatus() != null
-                && (natsConnection.getStatus().CONNECTED.equals(Status.CONNECTED)));
+                && (natsConnection.getStatus().equals(Connection.Status.CONNECTED)));
     }
 
     @Override
     public void publish(String stream, String subject, Object message) throws Exception {
         if (!isConnected()) {
-            throw new IllegalStateException("Gateway connection has not been established");
+            throw new IllegalStateException("Gateway connection has not been established.");
         }
         if (!shutdownInitiated) {
-            PublishingQueueTask task = new PublishingQueueTask(subject, message);
-            publishingQueue.put(task);
+            PUBLISHING_QUEUE.put(new PublishingQueueTask(subject, message));
         } else {
             LOG.error("Shutdown initiated, not accepting publish request: \n" + message);
             throw new IllegalStateException("Shutdown initiated, not accepting anymore publish requests");
@@ -184,28 +97,13 @@ public class JSGatewayImpl implements Gateway {
     public void subscribe(String stream, String subject, Class messageClass,
             MessageConsumer messageConsumer) throws Exception {
         if (!isConnected()) {
-            throw new IllegalStateException("Gateway connection has not been established");
+            throw new IllegalStateException("Gateway connection has not been established.");
         }
         if (!subscribers.containsKey(subject)) {
             Dispatcher dispatcher = natsConnection.createDispatcher();
-            MessageHandler handler = new MessageHandler() {
-                @Override
-                public void onMessage(io.nats.client.Message msg) throws InterruptedException {
-                    Object message = null;
-                    try {
-                        String json = new String(msg.getData(), StandardCharsets.UTF_8);
-                        message = mapper.readValue(json, messageClass);
-                    } catch (Exception e) {
-                        LOG.error("Error deserializing NATS message: \n" + msg);
-                        LOG.error("Exception: \n" + e.getMessage());
-                    }
-                    if (message != null) {
-                        messageConsumer.onMessage(message);
-                    }
-                }
-            };
             //Should we add PushSubscribeOptions to add durable name?
-            JetStreamSubscription sub = jsConnection.subscribe(subject, dispatcher, handler, false);
+            JetStreamSubscription sub = jsConnection.subscribe(subject, dispatcher,
+                msg -> onMessage(msg, messageClass, messageConsumer), false);
             subscribers.put(subject, sub);
         }
     }
@@ -217,11 +115,36 @@ public class JSGatewayImpl implements Gateway {
         }
         exec.shutdownNow();
         shutdownInitiated = true;
-        publishingShutdownLatch.await();
+        PUBLISHING_SHUTDOWN_LATCH.await();
         natsConnection.close();
     }
 
-    private String generatePublishFailureRecord(String subject, String message) {
+    /**
+     * Configure basic message handler logic.
+     * @param msg
+     * @param messageClass
+     * @param messageConsumer
+     */
+    public void onMessage(Message msg, Class messageClass, MessageConsumer messageConsumer) {
+        String payload = new String(msg.getData(), StandardCharsets.UTF_8);
+        Object message = null;
+
+        try {
+            message = mapper.readValue(payload, messageClass);
+        } catch (JsonProcessingException ex) {
+            LOG.error("Error deserializing NATS message: " + payload, ex);
+        }
+        if (message != null) {
+            messageConsumer.onMessage(message);
+        }
+    }
+
+    /**
+     * Writes to publishing logger file.
+     * @param subject
+     * @param message
+     */
+    public static void writeToPublishingLoggerFile(String subject, String message) {
         String currentDate = LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE);
         StringBuilder builder = new StringBuilder();
         builder.append(currentDate)
@@ -230,6 +153,28 @@ public class JSGatewayImpl implements Gateway {
                 .append("\t")
                 .append(message)
                 .append("\n");
-        return builder.toString();
+
+        try {
+            fileUtil.writeToFile(publishingLoggerFile,
+                    builder.toString());
+        } catch (IOException ex) {
+            LOG.error("Error during attempt to log publishing task to logger file", ex);
+        }
+    }
+
+    public static Boolean shutdownInitiated() {
+        return shutdownInitiated;
+    }
+
+    public static void countdownPublishingShutdownLatch() {
+        PUBLISHING_SHUTDOWN_LATCH.countDown();
+    }
+
+    public static PublishingQueueTask pollPublishingQueue() throws InterruptedException {
+        return PUBLISHING_QUEUE.poll(100, TimeUnit.MILLISECONDS);
+    }
+
+    public static Boolean publishingQueueIsEmpty() {
+        return PUBLISHING_QUEUE.isEmpty();
     }
 }
